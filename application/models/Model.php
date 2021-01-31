a91e6338fd38f6ea0ca787ab599ce03bfd376730
Ikhlasul Amal <ikhlasul0507@gmail.com>
2021-01-31 18:15:20 +0700

    hapus data di model

--- application/models/Model.php
+++ application/models/Model.php
@@ -48,37 +48,5 @@ class Model extends CI_Model{
 		$result=$this->db->update('tbl_gaji');
 		return $result;	
 	}
-	// function saveEmp(){
-	// 	$data = array(				
-	// 			'name' 			=> $this->input->post('name'), 
-	// 			'age' 			=> $this->input->post('age'), 
-	// 			'designation' 	=> $this->input->post('designation'), 
-	// 			'skills' 		=> $this->input->post('skills'), 
-	// 			'address' 		=> $this->input->post('address'), 
-	// 		);
-	// 	$result=$this->db->insert('emp',$data);
-	// 	return $result;
-	// }
-	// function updateEmp(){
-	// 	$id=$this->input->post('id');
-	// 	$name=$this->input->post('name');
-	// 	$age=$this->input->post('age');
-	// 	$designation=$this->input->post('designation');
-	// 	$skills=$this->input->post('skills');
-	// 	$address=$this->input->post('address');
-	// 	$this->db->set('name', $name);
-	// 	$this->db->set('age', $age);
-	// 	$this->db->set('designation', $designation);
-	// 	$this->db->set('skills', $skills);
-	// 	$this->db->set('address', $address);
-	// 	$this->db->where('id', $id);
-	// 	$result=$this->db->update('emp');
-	// 	return $result;	
-	// }
-	// function deleteEmp(){
-	// 	$id=$this->input->post('id');
-	// 	$this->db->where('id', $id);
-	// 	$result=$this->db->delete('emp');
-	// 	return $result;
-	// }	
+		
 }
